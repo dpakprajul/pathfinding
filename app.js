ec3be1da91938c55a954c9b15c1d466d546482f7
dpakprajul <38970123+dpakprajul@users.noreply.github.com>
2021-01-20 18:09:09 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,112 @@
+// default map layer
+let map = L.map('map', {
+    layers: MQ.mapLayer(),
+    center: [27.7172, 85.324],
+    zoom: 15
+});
+    
+
+    function runDirection(start, end) {
+        
+        // recreating new map layer after removal
+        map = L.map('map', {
+            layers: MQ.mapLayer(),
+            center: [27.7172, 85.324],
+            zoom: 12
+        });
+        
+        var dir = MQ.routing.directions();
+
+        dir.route({
+            locations: [
+                start,
+                end
+            ]
+        });
+    
+
+        CustomRouteLayer = MQ.Routing.RouteLayer.extend({
+            createStartMarker: (location) => {
+                var custom_icon;
+                var marker;
+
+                custom_icon = L.icon({
+                    iconUrl: 'img/red.png',
+                    iconSize: [20, 29],
+                    iconAnchor: [10, 29],
+                    popupAnchor: [0, -29]
+                });
+
+                marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
+
+                return marker;
+            },
+
+            createEndMarker: (location) => {
+                var custom_icon;
+                var marker;
+
+                custom_icon = L.icon({
+                    iconUrl: 'img/blue.png',
+                    iconSize: [20, 29],
+                    iconAnchor: [10, 29],
+                    popupAnchor: [0, -29]
+                });
+
+                marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
+
+                return marker;
+            }
+        });
+        
+        map.addLayer(new CustomRouteLayer({
+            directions: dir,
+            fitBounds: true
+        })); 
+    }
+
+
+// function that runs when form submitted
+function submitForm(event) {
+    event.preventDefault();
+
+    // delete current map layer
+    map.remove();
+
+    // getting form data
+    start = document.getElementById("start").value;
+    end = document.getElementById("destination").value;
+
+    // run directions function
+    runDirection(start, end);
+
+    // reset form
+    document.getElementById("form").reset();
+}
+
+// asign the form to form variable
+const form = document.getElementById('form');
+
+// call the submitForm() function when submitting the form
+form.addEventListener('submit', submitForm);
+
+map.locate({setView: true, maxZoom: 16});
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
+
+
+
